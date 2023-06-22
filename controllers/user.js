5d04325f337745dc299c402d7993cac3b18525a5
ayush kumar <aayush.21jdai066@jietjodhpur.ac.in>
2023-06-23 00:19:48 +0530

    initial commit

---
+++ controllers/user.js
@@ -0,0 +1,82 @@
+import { User } from "../models/user.js";
+import bcrypt from "bcrypt";
+import { sendCookie } from "../utils/features.js";
+
+
+//login cantroller
+export const login = async (req,res,next)=>{
+  try {
+    const {email, password } = req.body;
+
+
+  const user = await User.findOne({email}).select("+password");
+
+  if(!user) return next(new ErrorHandler("Invalid Email or password", 400));
+  
+
+
+  const isMatch = await bcrypt.compare(password,user.password);
+
+
+  if(!isMatch) return next(new ErrorHandler("Invalid Email or Password", 404));
+
+  sendCookie(user,res,`Welcome back, ${user.name}`, 200);
+  } catch (error) {
+
+    next(error);
+    
+  }
+
+};
+
+
+
+
+
+
+// register cantroller
+
+export const register = async (req,res)=>{
+
+    try {
+      const { name , email , password } = req.body;
+
+    let user = await User.findOne({email});
+
+
+    if(user) return next(new ErrorHandler("user Already Exist", 400)); 
+
+    const hashedPassword = await bcrypt.hash(password , 10);
+    user = await User.create({name,email, password: hashedPassword});
+    sendCookie(user,res,"Registered Successfully", 201);
+    } catch (error) {
+
+      next(error);
+      
+    }
+};
+
+
+
+
+export const getMyProfile = (req,res)=>{
+
+  res.status(200).json({
+    success: true,
+    user:req.user,
+  });
+
+};
+
+export const logout = (req,res)=>{
+  res.status(200).cookie("token",null,{
+    expires: new Date(Date.now()),
+    sameSite: process.env.NODE_ENV === "Development" ? "lax": "none",
+    secure: process.env.NODE_ENV === "Development" ? false: true,
+  }).json({
+    success: true,
+    user: req.user,
+  });
+
+};
+
---
+++ data/database.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+export const connectDB = ()=>{
+    mongoose
+    .connect(process.env.MONGO_URI , {
+    dbName: "backendwala",
+    })
+    .then(() => console.log("Database Connected"))
+    .catch((e) => console.log(e));
+};
+
