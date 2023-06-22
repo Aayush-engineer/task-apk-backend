4ab46d1f4c35e762f700b7043dba7b21f7c98201
ayush kumar <aayush.21jdai066@jietjodhpur.ac.in>
2023-06-23 00:52:17 +0530

    fixed engine

--- data/database.js
+++ data/database.js
@@ -5,7 +5,7 @@ export const connectDB = ()=>{
     .connect(process.env.MONGO_URI , {
     dbName: "backendwala",
     })
-    .then(() => console.log("Database Connected"))
+    .then((c) => console.log(`Database Connected with ${c.connection.host}`))
     .catch((e) => console.log(e));
 };
 
